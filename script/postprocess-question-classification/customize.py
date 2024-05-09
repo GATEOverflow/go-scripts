3a2e620dd9cd0a8e7a55fe5554c064334759bda6
ANANDHU S <71482562+anandhu-eng@users.noreply.github.com>
2024-05-09 15:23:54 +0530

    Added OpenAI evaluation support

--- script/postprocess-question-classification/customize.py
+++ script/postprocess-question-classification/customize.py
@@ -24,7 +24,11 @@ def postprocess(i):
             if(soln[i] in ast.literal_eval(model_soln[i])):
                 correct = correct + 1
         print("\tAccuracy by brute force approach is:"+str(correct/len(model_soln)))
-
+    elif(env['CM_ML_MODEL_NAME'] == "GPT3.5"):
+        modelSolnData=pd.read_csv(env['CM_ML_MODEL_ANSWER'],header=0)
+        accuracy = (modelSolnData['Tag'] == modelSolnData['predictedTags']).mean()
+        print(f"Accuracy through GPT3.5 is{accuracy}")
+        print(f"NOTE: The solution file is present in path: {env['CM_ML_MODEL_ANSWER']}")
     else:
         soln=pd.read_csv(env['CM_DATASET_SOLUTION_PATH'],header=0)["Tag"]
         model_soln=pd.read_csv(env['CM_ML_MODEL_ANSWER'],header=0)["PredictedLabels"]
