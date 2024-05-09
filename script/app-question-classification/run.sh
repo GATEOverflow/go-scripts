59bcc01b928d0c947a5484f69c8d49c1be6886c4
ANANDHU S <71482562+anandhu-eng@users.noreply.github.com>
2024-05-09 16:25:36 +0530

    Update run.sh

--- script/app-question-classification/run.sh
+++ script/app-question-classification/run.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 
+echo "$CM_PYTHON_BIN_WITH_PATH" "$CM_TMP_CURRENT_SCRIPT_PATH/process.py"
+"$CM_PYTHON_BIN_WITH_PATH" "$CM_TMP_CURRENT_SCRIPT_PATH/process.py"
+if [ $? -ne 0 ]; then
+    exit 1
+fi
