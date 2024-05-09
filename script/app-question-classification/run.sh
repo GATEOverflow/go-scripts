155cbcb16efcf619d1ff848b9327ca57e9910186
ANANDHU S <71482562+anandhu-eng@users.noreply.github.com>
2024-05-09 18:31:35 +0530

    Env key added

--- script/app-question-classification/run.sh
+++ script/app-question-classification/run.sh
@@ -5,3 +5,4 @@ echo "$CM_PYTHON_BIN_WITH_PATH" "$CM_TMP_CURRENT_SCRIPT_PATH/process.py"
 if [ $? -ne 0 ]; then
     exit 1
 fi
+echo "CM_ML_MODEL_ANSWER=$(pwd)/Predicted_answers.csv" > tmp-run-env.out
