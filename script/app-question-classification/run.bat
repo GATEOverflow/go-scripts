518056c14876b6f0713c9d8e8c1df8a136dc6da1
ANANDHU S <71482562+anandhu-eng@users.noreply.github.com>
2024-05-09 15:21:27 +0530

    added windows support

--- script/app-question-classification/run.bat
+++ script/app-question-classification/run.bat
@@ -2,4 +2,3 @@
 echo "%CM_PYTHON_BIN_WITH_PATH%" "%CM_TMP_CURRENT_SCRIPT_PATH%\process.py"
 "%CM_PYTHON_BIN_WITH_PATH%" "%CM_TMP_CURRENT_SCRIPT_PATH%\process.py"
 if %errorlevel% neq 0 exit /b 1
-echo CM_DATASET_TAGS=%CD%\tagList.json > tmp-run-env.out
