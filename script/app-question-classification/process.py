ad5237be736e1bb07389887945c6a8f0b6cbed1c
H9660 <hussainloha.wala@.fourkitescom>
2025-04-20 23:16:06 +0530

    removed_dup_code

--- script/app-question-classification/process.py
+++ script/app-question-classification/process.py
@@ -69,79 +69,10 @@ def getAnthropicresponse(anthropicClient, content, model):
     )
     formattedResponse = message.content[0].text
     return formattedResponse
-
-
-if(os.environ['MLC_ML_MODEL_NAME'] == "go_2"):
-    from setfit import SetFitModel
-    import torch
-    dataset = load_dataset("ANANDHU-SCT/TOPIC_CLASSIFICATION")
-    model = SetFitModel.from_pretrained(os.environ['MLC_ML_MODEL'])
-    probs = model.predict_proba(dataset['test']['Question'])
-    final_result = []
-    resultfile = pd.DataFrame()
-    resultfile["Question"] = dataset["test"]["Question"]
-    resultfile["Tag"] = dataset["test"]["Tag"]
-    resultfile["Actual soln"] = dataset["test"]["label"]
-    for prob in probs:
-        print(type(prob))
-        try:
-            topk_values, topk_indices = torch.topk(torch.from_numpy(prob), k=5)
-        except:    
-            topk_values, topk_indices = torch.topk(prob, k=5)
-        # print(torch.argmax(prob, dim=0)
-        final_result.append(topk_indices.tolist())
-    resultfile["PredictedLabels"] = final_result
-    resultfile.to_csv('Predicted_answers.csv')
-
-    # print(probs)
-elif(os.environ['MLC_ML_MODEL_PLATFORM'] == "CLAUDE"):
+def getLLMResponse():
     testfile = get_data_file(os.environ['MLC_DATASET_SOLUTION_PATH'])
     tagListPath = os.environ["MLC_DATASET_TAGS"]
     tagListPath=r"{}".format(tagListPath)
-    anthropicClient = getAnthropicClient(os.environ["ANTHROPIC_API_KEY"])
-    with open(tagListPath, 'r') as file:
-        # Load the JSON data
-        data = json.load(file)
-    # Obtain the value of the key
-    tagList = None
-    for key in data:
-        tagList = data[key]
-    predictedTagList = []
-    for question in tqdm(testfile['Question']):
-        fewShotPrompt = f"""
-        You are an expert topic classifier. Your role is to analyse a question and classify the question into any of the following question Tags:
-        {tagList}
-        The steps to infer the answer would be:
-        1. Analyse the question    
-        2. Infer the topic to which the question belongs.
-        3. Check if the question belongs to any of the specific area within the infered topic. eg; if the topic to which the question belongs is arrays and on further analysis, if the question can be specifically associated with subtopic such as array multipliers (which comes within the topic array), then the topic to be asssigned to the question is array multiplier.
-        4. If any specific subtopic is found , return that as answer else return the topic as the answer.
-        5. return only the answer as string , dont explain.
-
-        Here are some examples:
-
-        input: The expression large frac x y x y 2 is equal to The maximum of x and y The minimum of x and y 1 None of the above 
-        output: Absolute Value
-
-        input: The number of full and half adders required to add 16 bit numbers is 8 half adders 8 full adders 1 half adder 15 full adders 16 half adders 0 full adders 4 half adders 12 full adders  
-        output: Adder
-
-        input: Suppose a fair six sided die is rolled once If the value on the die is 1 2 or 3 the die is rolled a second time What is the probability that the sum total of values that turn up is at least 6 dfrac 10 21 dfrac 5 12 dfrac 2 3 dfrac 1 6 
-        output: Bayers Theorem
-
-        input: {question}
-        output:
-        """
-        response = getAnthropicresponse(anthropicClient, fewShotPrompt, model=os.environ["MLC_ML_MODEL_NAME"])
-        predictedTagList.append(response)
-    testfile['predictedTags'] = predictedTagList
-    testfile.to_csv(os.path.join(os.getcwd(),'Predicted_answers.csv'))
-    
-elif(os.environ['MLC_ML_MODEL_PLATFORM'] == "OPENAI"):
-    testfile = get_data_file(os.environ['MLC_DATASET_SOLUTION_PATH'])
-    tagListPath = os.environ["MLC_DATASET_TAGS"]
-    tagListPath=r"{}".format(tagListPath)
-    openAIClient = getOpenAIClient(os.environ["OPENAI_API_KEY"])
     with open(tagListPath, 'r') as file:
         # Load the JSON data
         data = json.load(file)
@@ -179,6 +110,36 @@ elif(os.environ['MLC_ML_MODEL_PLATFORM'] == "OPENAI"):
         predictedTagList.append(response)
     testfile['predictedTags'] = predictedTagList
     testfile.to_csv(os.path.join(os.getcwd(),'Predicted_answers.csv'))
+
+if(os.environ['MLC_ML_MODEL_NAME'] == "go_2"):
+    from setfit import SetFitModel # this is used to quickly 
+    import torch
+    dataset = load_dataset("ANANDHU-SCT/TOPIC_CLASSIFICATION")
+    model = SetFitModel.from_pretrained(os.environ['MLC_ML_MODEL'])
+    probs = model.predict_proba(dataset['test']['Question'])
+    final_result = []
+    resultfile = pd.DataFrame()
+    resultfile["Question"] = dataset["test"]["Question"]
+    resultfile["Tag"] = dataset["test"]["Tag"]
+    resultfile["Actual soln"] = dataset["test"]["label"]
+    for prob in probs:
+        print(type(prob))
+        try:
+            topk_values, topk_indices = torch.topk(torch.from_numpy(prob), k=5)
+        except:    
+            topk_values, topk_indices = torch.topk(prob, k=5)
+        # print(torch.argmax(prob, dim=0)
+        final_result.append(topk_indices.tolist())
+    resultfile["PredictedLabels"] = final_result
+    resultfile.to_csv('Predicted_answers.csv')
+
+    # print(probs)
+elif(os.environ['MLC_ML_MODEL_PLATFORM'] == "CLAUDE"):
+    anthropicClient = getAnthropicClient(os.environ["ANTHROPIC_API_KEY"])
+    getLLMResponse()
+elif(os.environ['MLC_ML_MODEL_PLATFORM'] == "OPENAI"):
+    openAIClient = getOpenAIClient(os.environ["OPENAI_API_KEY"])
+    getLLMResponse()
 else:
     testfile = get_data_file(os.environ['MLC_PREPROCESSED_DATASET_TEST_PATH'])
     ans_list = get_trainfile_solnval(os.environ['MLC_PREPROCESSED_DATASET_TRAIN_PATH'])
@@ -206,3 +167,4 @@ else:
     testfile['PredictedLabels'] = solutions
     testfile['Probabilities'] = main_list
     testfile.to_csv('Predicted_answers.csv')
+
